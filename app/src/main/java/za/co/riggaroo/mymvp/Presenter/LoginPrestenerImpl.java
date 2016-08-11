6d95c2fc4193b091bd618672fd6966da1d945402
roy989898 <roy989898@gmail.com>
2016-08-11 20:34:25 +0800

    feat:implement the method

--- app/src/main/java/za/co/riggaroo/mymvp/Presenter/LoginPrestenerImpl.java
+++ app/src/main/java/za/co/riggaroo/mymvp/Presenter/LoginPrestenerImpl.java
@@ -1,16 +1,43 @@
 package za.co.riggaroo.mymvp.Presenter;
 
+import za.co.riggaroo.mymvp.View.LoginView;
+
 /**
  * Created by User on 11/8/2016.
  */
 public class LoginPrestenerImpl implements LoginPrestener {
+    private LoginView loginVIew;
+
+    public LoginPrestenerImpl(LoginView loginVIew) {
+        this.loginVIew = loginVIew;
+    }
+
     @Override
     public void vaildLogin(String name, String password) {
+        
+
+    }
+
+    @Override
+    public void loginSuccess() {
+        if(loginVIew!=null){
+            loginVIew.showSuccesssMessage();
+            loginVIew.hideProgress();
+        }
+
+    }
+
+    @Override
+    public void loginFail() {
+        if(loginVIew!=null){
+            loginVIew.showErrorMessage();
+            loginVIew.hideProgress();
+        }
 
     }
 
     @Override
     public void onDestory() {
-
+        loginVIew = null;
     }
 }
