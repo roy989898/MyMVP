f4bf3f7d7114ae2082ff03d0ed1c4a9c7aa10d30
roy989898 <roy989898@gmail.com>
2016-08-11 20:09:12 +0800

    feat:create the Login present

---
+++ app/src/main/java/za/co/riggaroo/mymvp/Presenter/LoginPrestener.java
@@ -0,0 +1,10 @@
+package za.co.riggaroo.mymvp.Presenter;
+
+/**
+ * Created by User on 11/8/2016.
+ */
+public interface LoginPrestener{
+    void vaildLogin(String name,String password);
+
+    void onDestory();
+}
---
+++ app/src/main/java/za/co/riggaroo/mymvp/Presenter/LoginPrestenerImpl.java
@@ -0,0 +1,16 @@
+package za.co.riggaroo.mymvp.Presenter;
+
+/**
+ * Created by User on 11/8/2016.
+ */
+public class LoginPrestenerImpl implements LoginPrestener {
+    @Override
+    public void vaildLogin(String name, String password) {
+
+    }
+
+    @Override
+    public void onDestory() {
+
+    }
+}
