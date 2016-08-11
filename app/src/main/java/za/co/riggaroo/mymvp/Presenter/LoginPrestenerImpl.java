e9bc293299687df4522480364444ecbd00f4ff8a
roy989898 <roy989898@gmail.com>
2016-08-11 22:40:52 +0800

    feat:implement the Login Present

--- app/src/main/java/za/co/riggaroo/mymvp/Presenter/LoginPrestenerImpl.java
+++ app/src/main/java/za/co/riggaroo/mymvp/Presenter/LoginPrestenerImpl.java
@@ -1,19 +1,27 @@
 package za.co.riggaroo.mymvp.Presenter;
 
+import za.co.riggaroo.mymvp.Model.LoginModelImpl;
 import za.co.riggaroo.mymvp.View.LoginView;
 
 /**
  * Created by User on 11/8/2016.
  */
 public class LoginPrestenerImpl implements LoginPrestener {
+    private final LoginModelImpl loginModel;
     private LoginView loginVIew;
 
     public LoginPrestenerImpl(LoginView loginVIew) {
         this.loginVIew = loginVIew;
+        loginModel=new LoginModelImpl(this);
     }
 
     @Override
     public void vaildLogin(String name, String password) {
+        if(loginVIew!=null){
+            loginVIew.showProgress();
+
+        }
+        loginModel.login(name,password);
         
 
     }
@@ -23,6 +31,8 @@ public class LoginPrestenerImpl implements LoginPrestener {
         if(loginVIew!=null){
             loginVIew.showSuccesssMessage();
             loginVIew.hideProgress();
+
+            loginVIew.toMain();
         }
 
     }
